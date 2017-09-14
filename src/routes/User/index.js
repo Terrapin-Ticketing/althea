515a5261eae790cb3243a1a2dfa5b7fefbda2f70
Micheal Reeder <micheal@wyzerr.com>
2017-09-14 14:17:21 -0400

    user

--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -1,24 +1,24 @@
-import { injectReducer } from '../../store/reducers'
+import { injectReducer } from '../../store/reducers';
 
 export default (store) => ({
-  path : 'user',
+  path: 'user',
   /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
+  getComponent(nextState, cb) {
     /*  Webpack - use 'require.ensure' to create a split point
         and embed an async module loader (jsonp) when bundling   */
     require.ensure([], (require) => {
       /*  Webpack - use require callback to define
           dependencies for bundling   */
-      const Events = require('./containers/UserContainer').default
-      const reducer = require('./modules/user').default
+      const Events = require('./containers/UserContainer').default;
+      const reducer = require('./modules/user').default;
 
       /*  Add the reducer to the store on key 'login'  */
-      injectReducer(store, { key: 'user', reducer })
+      injectReducer(store, { key: 'user', reducer });
 
       /*  Return getComponent   */
-      cb(null, Events)
+      cb(null, Events);
 
     /* Webpack named bundle   */
-  }, 'user')
+    }, 'user');
   }
-})
+});
