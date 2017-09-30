4ed7c3250cf5da6a45bf421b7ced3200ffb000ea
Michael Reeder <reeder@knoesis.org>
2017-09-30 15:13:19 -0400

    put page layout back in because who knew where it when?

---
+++ src/layouts/PageLayout/PageLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
+import PropTypes from 'prop-types';
+import Header from '../Header';
+import './PageLayout.scss';
+
+export default (store) => {
+  const PageLayout = ({ children }) => (
+    <div className='container text-center'>
+      <Header store={store}/>
+      <div className='page-layout__viewport'>
+        {children}
+      </div>
+    </div>
+  );
+
+  PageLayout.propTypes = {
+    children: PropTypes.node
+  };
+
+  return PageLayout;
+};
