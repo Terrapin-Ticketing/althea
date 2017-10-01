c1525ea43e9cf14c6f78d71b7a3b906c1a74478e
Michael Reeder <reeder@knoesis.org>
2017-10-01 00:28:19 -0400

    minor cosmetic stuff

--- src/layouts/Navigation/index.js
+++ src/layouts/Navigation/index.js
@@ -23,7 +23,7 @@ export const Navigation = (props) => {
         <input className="burger-check" id="burger-check" type="checkbox" />
         <label htmlFor="burger-check" className="burger"></label>
         <IndexLink to='/'><img className="nav-logo" src={require('../assets/img/logo-square.png')} /></IndexLink>
-        <span className="filler"></span>
+        <label htmlFor="burger-check" className="burger-space"></label>
         <div className='navigation'>
           {(!user) ? (<span className='nav-item'><Link to='/signup' className='nav-item' activeClassName='page-layout__nav-item--active'>Signup</Link></span>) : null}
           {(!user) ? (<span className='nav-item'><Link to='/login' className='nav-item' activeClassName='page-layout__nav-item--active'>Login</Link></span>): null}
