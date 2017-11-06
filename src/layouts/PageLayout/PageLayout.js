37f09222161c7120bb804294cfd9b503dfded8e3
Kevin Mulcrone <kaybesee@gmail.com>
2017-11-06 10:54:56 -0800

    fixed login bug with pagelayout not changing state

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { IndexLink, Link, browserHistory } from 'react-router';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 // import Sidebar from '../Sidebar';
 import './PageLayout.scss';
 
-export default (store) => {
-  const { user } = store.getState().auth;
 
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.user
+  };
+};
+
+export default (store) => {
   const logout = () => {
     store.dispatch({
       type: 'LOGOUT',
@@ -15,35 +21,37 @@ export default (store) => {
     browserHistory.push('login');
   };
 
-  const PageLayout = ({ children }) => {
-    return (
-      <div className='container'>
-        <div className="top-bar">
-          <div className="left-column">
-            <IndexLink to='/'><img style={{width: 300}} src={require('../assets/img/logo-side-text.png')} /></IndexLink>
+  const PageLayout = class PageLayout extends Component {
+    render() {
+      const { user } = this.props;
+      return (
+        <div className='container'>
+          <div className="top-bar">
+            <div className="left-column">
+              <IndexLink to='/'><img style={{width: 300}} src={require('../assets/img/logo-side-text.png')} /></IndexLink>
+            </div>
+            <div className="right-column">
+              <Link to='/events' activeClassName='active'>Events</Link>
+              <Link to='/createEvent' activeClassName='active'>Create Event</Link>
+              {(user) ? <Link to='/user' activeClassName='active'>My Account</Link> : null }
+              {(!user) ? <Link to='/login' activeClassName='active'>Login</Link> : null }
+              {(user) ? <Link style={{cursor: 'pointer'}} onClick={() => logout()}>Logout</Link> : null }
+              {/* <Link to='#'>Help</Link> */}
+            </div>
           </div>
-          <div className="right-column">
-            <Link to='/events' activeClassName='active'>Events</Link>
-            <Link to='/createEvent' activeClassName='active'>Create Event</Link>
-            {(user) ? <Link to='/user' activeClassName='active'>My Account</Link> : null }
-            {(!user) ? <Link to='/login' activeClassName='active'>Sign Up</Link> : null }
-            {(user) ? <Link style={{cursor: 'pointer'}} onClick={() => logout()}>Logout</Link> : null }
-            <Link to='#'>Help</Link>
+          <div className="page-container">
+            {/* <Sidebar store={store}/> */}
+            <div className='page-content'>
+              {this.props.children}
+            </div>
           </div>
         </div>
-        <div className="page-container">
-          {/* <Sidebar store={store}/> */}
-          <div className='page-content'>
-            {children}
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   };
 
   PageLayout.propTypes = {
     children: PropTypes.node
   };
-
-  return PageLayout;
+  return connect(mapStateToProps, { })(PageLayout);
 };
