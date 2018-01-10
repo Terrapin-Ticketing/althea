3e773ee5f22077231b49ef8e24631604e717ff9a
Michael Reeder <reeder@knoesis.org>
2018-01-10 15:48:20 -0500

    added resonsone to events page

--- src/routes/Event/components/Event.js
+++ src/routes/Event/components/Event.js
@@ -84,8 +84,9 @@ class Event extends Component {
   }
 
   renderActivateButton() {
+    console.log(this.props.event.urlSafe);
     return (
-      <button onClick={()=> browserHistory.push(`${this.props.urlSafe}/activate`)} className="waves-effect waves-light btn-large terrapin-green">Activate Tickets</button>
+      <button onClick={()=> browserHistory.push(`${this.props.event.urlSafe}/activate`)} className="waves-effect waves-light btn-large terrapin-green">Activate Tickets</button>
     );
   }
 
