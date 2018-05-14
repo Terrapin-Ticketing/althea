9879bc9e8c61c0acf68e7be0f070f28ed5698e3d
Kevin Mulcrone <kaybesee@gmail.com>
2018-05-14 14:41:50 -0400

    bug fix

--- src/routes/Event/components/Event.js
+++ src/routes/Event/components/Event.js
@@ -150,7 +150,7 @@ class Event extends Component {
               </div>
               <div className="right-column">
                 <h3>Date</h3>
-                March 2nd - 3rd, 2018
+                May 17th - 19th, 2018
 
                 <h3>Location</h3>
                   {venue.name} <br />
